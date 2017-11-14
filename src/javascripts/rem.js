9b5ffb46ee90d004b80c986c8e79f97f3ad83e00
祗堂幻夜 <dogamemaster@sina.cn>
2017-11-14 11:08:20 +0800

    22

--- src/javascripts/rem.js
+++ src/javascripts/rem.js
@@ -2,8 +2,10 @@
 (function() {
     var ft = document.getElementsByTagName("html")[0]; //获取到html标签
     var s = window.screen.width; //获取屏幕的宽度
+    console.log(s)
     window.onresize = function() { //屏幕尺寸改变触发
         var w = document.body.offsetWidth; //获取浏览器内容的宽度
+        console.log(w)
         ft.style.fontSize = w / s * 16 + "px";
     }
 })()
